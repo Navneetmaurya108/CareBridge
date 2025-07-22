ca4ae82f8a4843bc65912670cd9b088000c279c6
Navneet Maurya <navneetmaurya108@gmail.com>
2025-07-22 22:25:10 +0530

    add api to get user appointments

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -202,3 +202,18 @@ const cancelAppointment = async (req, res) => {
         res.json({ success: false, message: error.message })
     }
 }
+
+// API to get user appointments for frontend my-appointments page
+const listAppointment = async (req, res) => {
+    try {
+
+        const { userId } = req.body
+        const appointments = await appointmentModel.find({ userId })
+
+        res.json({ success: true, appointments })
+
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
+}
