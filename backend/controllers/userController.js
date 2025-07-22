eda8d4136449bba7640c2e111d6e0b9470308c9a
Navneet Maurya <navneetmaurya108@gmail.com>
2025-07-22 22:21:18 +0530

    add api to update user profile

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -82,3 +82,34 @@ const getProfile = async (req, res) => {
         res.json({ success: false, message: error.message })
     }
 }
+
+// API to update user profile
+const updateProfile = async (req, res) => {
+
+    try {
+
+        const { userId, name, phone, address, dob, gender } = req.body
+        const imageFile = req.file
+
+        if (!name || !phone || !dob || !gender) {
+            return res.json({ success: false, message: "Data Missing" })
+        }
+
+        await userModel.findByIdAndUpdate(userId, { name, phone, address: JSON.parse(address), dob, gender })
+
+        if (imageFile) {
+
+            // upload image to cloudinary
+            const imageUpload = await cloudinary.uploader.upload(imageFile.path, { resource_type: "image" })
+            const imageURL = imageUpload.secure_url
+
+            await userModel.findByIdAndUpdate(userId, { image: imageURL })
+        }
+
+        res.json({ success: true, message: 'Profile Updated' })
+
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
+}
