1b78f5cde62e1cb351b7eef202b8186bd621402a
Navneet Maurya <navneetmaurya108@gmail.com>
2025-07-23 20:30:28 +0530

    api tomake payment of appointment

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -217,3 +217,32 @@ const listAppointment = async (req, res) => {
         res.json({ success: false, message: error.message })
     }
 }
+
+// API to make payment of appointment using razorpay
+const paymentRazorpay = async (req, res) => {
+    try {
+
+        const { appointmentId } = req.body
+        const appointmentData = await appointmentModel.findById(appointmentId)
+
+        if (!appointmentData || appointmentData.cancelled) {
+            return res.json({ success: false, message: 'Appointment Cancelled or not found' })
+        }
+
+        // creating options for razorpay payment
+        const options = {
+            amount: appointmentData.amount * 100,
+            currency: process.env.CURRENCY,
+            receipt: appointmentId,
+        }
+
+        // creation of an order
+        const order = await razorpayInstance.orders.create(options)
+
+        res.json({ success: true, order })
+
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
+}
