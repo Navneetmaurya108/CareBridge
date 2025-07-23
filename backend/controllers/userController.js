6db70d498b8dbc830503cc4374a816c6a04e9aad
Navneet Maurya <navneetmaurya108@gmail.com>
2025-07-23 20:49:01 +0530

    add necessary packages

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,3 +1,13 @@
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import validator from "validator";
+import userModel from "../models/userModel.js";
+import doctorModel from "../models/doctorModel.js";
+import appointmentModel from "../models/appointmentModel.js";
+import { v2 as cloudinary } from 'cloudinary'
+import stripe from "stripe";
+import razorpay from 'razorpay';
+
 // Gateway Initialize
 const stripeInstance = new stripe(process.env.STRIPE_SECRET_KEY)
 const razorpayInstance = new razorpay({
