38446e2b2e6afb327b7575f40cbb511fe453cf76
Navneet Maurya <navneetmaurya108@gmail.com>
2025-07-23 20:31:49 +0530

    api to verify payment

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -246,3 +246,22 @@ const paymentRazorpay = async (req, res) => {
         res.json({ success: false, message: error.message })
     }
 }
+
+// API to verify payment of razorpay
+const verifyRazorpay = async (req, res) => {
+    try {
+        const { razorpay_order_id } = req.body
+        const orderInfo = await razorpayInstance.orders.fetch(razorpay_order_id)
+
+        if (orderInfo.status === 'paid') {
+            await appointmentModel.findByIdAndUpdate(orderInfo.receipt, { payment: true })
+            res.json({ success: true, message: "Payment Successful" })
+        }
+        else {
+            res.json({ success: false, message: 'Payment Failed' })
+        }
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
+}
