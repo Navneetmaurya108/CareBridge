cd85d6e0c0564ae8ef08bdc5bb9f2c14581c597f
Navneet Maurya <navneetmaurya108@gmail.com>
2025-07-23 20:54:33 +0530

    api to cancel doctor appointment

--- backend/controllers/doctorController.js
+++ backend/controllers/doctorController.js
@@ -41,3 +41,24 @@ const appointmentsDoctor = async (req, res) => {
         res.json({ success: false, message: error.message })
     }
 }
+
+// API to cancel appointment for doctor panel
+const appointmentCancel = async (req, res) => {
+    try {
+
+        const { docId, appointmentId } = req.body
+
+        const appointmentData = await appointmentModel.findById(appointmentId)
+        if (appointmentData && appointmentData.docId === docId) {
+            await appointmentModel.findByIdAndUpdate(appointmentId, { cancelled: true })
+            return res.json({ success: true, message: 'Appointment Cancelled' })
+        }
+
+        res.json({ success: false, message: 'Appointment Cancelled' })
+
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
+
+}
