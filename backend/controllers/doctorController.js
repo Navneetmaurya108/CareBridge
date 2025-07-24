71057a7e6a6dfe6e8e7811418844e3b253cf5bcc
Navneet Maurya <navneetmaurya108@gmail.com>
2025-07-24 15:05:15 +0530

    add api to change doctor availability

--- backend/controllers/doctorController.js
+++ backend/controllers/doctorController.js
@@ -93,5 +93,20 @@ const doctorList = async (req, res) => {
         console.log(error)
         res.json({ success: false, message: error.message })
     }
+}
 
+// API to change doctor availablity for Admin and Doctor Panel
+const changeAvailablity = async (req, res) => {
+    try {
+
+        const { docId } = req.body
+
+        const docData = await doctorModel.findById(docId)
+        await doctorModel.findByIdAndUpdate(docId, { available: !docData.available })
+        res.json({ success: true, message: 'Availablity Changed' })
+
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
 }
