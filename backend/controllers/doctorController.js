6202a0bb7857487b7d4a529496408401cc840cf3
Navneet Maurya <navneetmaurya108@gmail.com>
2025-07-24 15:04:05 +0530

    api to get all doctors list

--- backend/controllers/doctorController.js
+++ backend/controllers/doctorController.js
@@ -81,3 +81,17 @@ const appointmentComplete = async (req, res) => {
         res.json({ success: false, message: error.message })
     }
 }
+
+// API to get all doctors list for Frontend
+const doctorList = async (req, res) => {
+    try {
+
+        const doctors = await doctorModel.find({}).select(['-password', '-email'])
+        res.json({ success: true, doctors })
+
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
+
+}
